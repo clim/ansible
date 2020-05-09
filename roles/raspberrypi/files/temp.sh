1d242d5e420f4058968f64e1075df67456318e2e
Chris Lim <christopher.a.lim@gmail.com>
2020-05-09 16:38:41 +0800

    Updated raspberry pi scripts for the Ubuntu 20.0

--- roles/raspberrypi/files/temp.sh
+++ roles/raspberrypi/files/temp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Raspbian
-vcgencmd measure_temp
+# vcgencmd measure_temp
 
 # Ubuntu
-#cat /sys/class/thermal/thermal_zone*/temp | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
+cat /sys/class/thermal/thermal_zone*/temp | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
